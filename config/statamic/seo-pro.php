f2afda7d58054be15619e8b04465349931f692d0
Andrew Topping <andrew@thoughtcollective.com>
2022-11-08 11:07:49 +0000

    Remove SEOTamic and bring in SEOPro

---
+++ config/statamic/seo-pro.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+    'assets' => [
+        'container' => 'assets',
+        'twitter_preset' => [
+            'w' => 1200,
+            'h' => 600,
+        ],
+        'open_graph_preset' => [
+            'w' => 1146,
+            'h' => 600,
+        ],
+    ],
+
+    'sitemap' => [
+        'enabled' => true,
+        'url' => 'sitemap.xml',
+        'expire' => 60,
+    ],
+
+    'humans' => [
+        'enabled' => true,
+        'url' => 'humans.txt',
+    ],
+
+    'pagination' => [
+        'enabled_in_canonical_url' => true,
+        'enabled_on_first_page' => false,
+    ],
+
+    'twitter' => [
+        'card' => 'summary_large_image',
+    ],
+
+    'alternate_locales' => [
+        'enabled' => true,
+        'excluded_sites' => [],
+    ],
+
+];
