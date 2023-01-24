a92ad1256f38a167aa596590c699161a95db1735
Andrew Topping <andrew@thoughtcollective.com>
2023-01-24 10:44:27 +0000

    Rename env to env.thoughtco and add key via Install Hook

--- StarterKitPostInstall.php
+++ StarterKitPostInstall.php
@@ -10,15 +10,17 @@ class StarterKitPostInstall
 
         $originalAppName = env('APP_URL');
         $originalAppUrl = env('APP_URL');
+        $originalAppKey = env('APP_KEY');
 
         $appName = $console->ask('What should be your app name?');
         $appName = preg_replace('/([\'|\"|#])/m', '', $appName);
 
         $appURL = $console->ask('What is the app url?');
 
-        $env = app('files')->get(base_path('.env'));
+        $env = app('files')->get(base_path('.env.thoughtco'));
         $env = str_replace("APP_NAME=", "APP_NAME=\"{$appName}\"", $env);
         $env = str_replace('APP_URL=', "APP_URL=\"{$appURL}\"", $env);
+        $env = str_replace('APP_KEY=', "APP_KEY=\"{$originalAppKey}\"", $env);
 
         app('files')->put(base_path('.env'), $env);
 
