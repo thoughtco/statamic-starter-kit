f8fe6d485bcf837d55e465253bbc38c3cc0b7c7a
Andrew Topping <andrew@thoughtcollective.com>
2023-11-16 10:30:10 +0000

    Update Starter Kit for TC Client Dashboard

---
+++ config/thoughtco/client-dashboard.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    'nav' => [
+        'section' => 'Thought Collective',
+        'title' => 'Resources',
+    ],
+
+    'trello_url' => '',
+
+    /**
+     * Configure Loom videos to be displayed on the dashboard.
+     *
+     * The embed_url is the *almost* the same as the sharing link Loom gives you.
+     * Except instead of `/share/`, it's `/embed`.
+     */
+    'looms' => [
+        [
+            'name' => '',
+            'embed_url' => '',
+        ],
+    ],
+
+    'additional_resources' => [
+        // [
+        //     'name' => '',
+        //     'url' => ',
+        // ],
+    ],
+
+];
