5903337a954493fcbd7548818e3de9e6a9aaee0b
Andrew Topping <andrew@thoughtcollective.com>
2024-01-17 15:47:27 +0000

    Remove addition of TC_PANEL_CACHE in .env

--- StarterKitPostInstall.php
+++ StarterKitPostInstall.php
@@ -22,10 +22,6 @@ class StarterKitPostInstall
         $env = str_replace("APP_NAME=", "APP_NAME=\"{$appName}\"", $env);
         $env = str_replace('APP_URL=', "APP_URL=\"{$appURL}\"", $env);
         $env = str_replace('APP_KEY=', "APP_KEY=\"{$originalAppKey}\"", $env);
-        $env .= '
-        # do we want to cache the default.tpl panels
-        TC_PANEL_CACHE=false
-        ';
 
         // output to console
         $console->info('<info>[✓]</info> generate env');
