eba9bcf7a6b5109886622a7ff27682d6eca5cc69
Andrew Topping <andrew@thoughtcollective.com>
2024-01-24 15:14:12 +0000

    Remove routes/web.php comment block and change env() to config()

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 // if we're in staging
 // we can access the cutup
-if (env('APP_ENV') == 'staging'){
+if (config('APP_ENV') == 'staging'){
     Route::get('cutup/{page}', function($page) {
         return view("cutup.{$page}");
     });
