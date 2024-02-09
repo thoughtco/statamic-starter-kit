eaf50a6b56d518d8f98a9e82bb01d726b294d741
Ryan Mitchell <ryan@thoughtcollective.com>
2024-02-09 12:36:30 +0000

    Add default js

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,5 +1,10 @@
 import Alpine from 'alpinejs'
+import Precognition from 'laravel-precognition-alpine';
+import Swiper from 'swiper';
+import 'swiper/css/swiper.css';
 
-window.Alpine = Alpine
+window.Alpine = Alpine;
+window.Swiper = Swiper;
 
+Alpine.plugin(Precognition);
 Alpine.start();
