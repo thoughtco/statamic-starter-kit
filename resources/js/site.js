f08886a2334d595fa6103fd1029163855c578c7f
Ryan Mitchell <ryan@thoughtcollective.com>
2024-07-25 11:32:42 +0100

    Update JS to recent approaches

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,10 +1,22 @@
-import Alpine from 'alpinejs'
+import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
+import intersect from '@alpinejs/intersect';
 import Precognition from 'laravel-precognition-alpine';
-import Swiper from 'swiper';
-import 'swiper/css/swiper.css';
+import { swiffyslider } from 'swiffy-slider';
+
+import "swiffy-slider/css";
 
 window.Alpine = Alpine;
-window.Swiper = Swiper;
+window.swiffyslider = swiffyslider;
 
 Alpine.plugin(Precognition);
-Alpine.start();
+Alpine.plugin(intersect);
+
+if (window.livewireScriptConfig?.csrf === 'STATAMIC_CSRF_TOKEN') {
+    document.addEventListener('statamic:nocache.replaced', () => Livewire.start());
+} else {
+    Livewire.start();
+}
+
+window.addEventListener('load', () => {
+    window.swiffyslider.init();
+});
